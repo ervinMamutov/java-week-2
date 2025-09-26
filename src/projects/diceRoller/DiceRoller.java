bd3dc228b41a6f81402897caae6505bf6df0453f
Ervin Mamutov <ervin.news@gmail.com>
2025-09-26 15:49:24 +0200

    Complited Dice Roller

--- src/projects/diceRoller/DiceRoller.java
+++ src/projects/diceRoller/DiceRoller.java
@@ -1,4 +1,37 @@
 package projects.diceRoller;
+import projects.arrayStatistics.UserInput;
 
 public class DiceRoller {
+    public static  void main(String[] args) {
+        App app = new App();
+
+        app.run();
+    }
 }
+
+class App {
+
+    UserInput input = new UserInput();
+
+
+    public void run() {
+        System.out.print("Enter how many times you want to roll the dice: ->  ");
+        int times = input.input();
+        int[] theDiceRoll = new int[times];
+    int sum = 0;
+        for (int i = 0; i < times; i++) {
+            theDiceRoll[i] = rollDice();
+            sum += theDiceRoll[i];
+            System.out.println((i + 1) + " roll of the dice: -> " + theDiceRoll[i]);
+        }
+        System.out.print("The total score: " + sum);
+    }
+
+    public int rollDice() {
+        return (int)(Math.random() * 6) + 1;
+    }
+
+
+
+}
+
