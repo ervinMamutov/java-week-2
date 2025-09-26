e47d0d86ab38bf8a113a563fa1683312c1455c5c
Ervin Mamutov <ervin.news@gmail.com>
2025-09-26 15:48:41 +0200

    Complited Book Information

--- src/projects/bookInformation/BookInformation.java
+++ src/projects/bookInformation/BookInformation.java
@@ -1,4 +1,32 @@
 package projects.bookInformation;
 
 public class BookInformation {
+    public static void main(String[] args) {
+        Book bk1 = new Book("James", "Percival Everett", 2025);
+        Book bk2 = new Book("All Fours", "Miranda July", 2024);
+
+        bk1.doPrintDetails();
+        bk2.doPrintDetails();
+
+    }
 }
+
+class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    protected void doPrintDetails() {
+        System.out.println("This book, '" + title + "', was written by " + author + " in " + year);
+    }
+}
+
+
+
+
